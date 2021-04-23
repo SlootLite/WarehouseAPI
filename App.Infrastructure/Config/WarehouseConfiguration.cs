4281747c4770ee06f42d2e48fffeef211a486df0
mitinmi <slootlite@yandex.ru>
2021-04-24 06:22:21 +0700

    Add project files.

---
+++ App.Infrastructure/Config/WarehouseConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using App.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infrastructure.Config
+{
+    public class WarehouseConfiguration : IEntityTypeConfiguration<Warehouse>
+    {
+        public void Configure(EntityTypeBuilder<Warehouse> builder)
+        {
+            builder.Property(s => s.Id)
+                .IsRequired()
+                .UseIdentityColumn();
+
+            builder.Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(300);
+        }
+    }
+}
---
+++ App.Infrastructure/Data/WarehouseContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App.Domain.Entities;
+using System.Reflection;
+
+namespace App.Infrastructure.Data
+{
+    public class WarehouseContext : DbContext
+    {
+        public WarehouseContext(DbContextOptions<WarehouseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Warehouse> Warehouses { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
